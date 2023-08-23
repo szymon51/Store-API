751ce46d122e3ceef2cb95d12cafadd1460e16fb
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-08-23 16:58:26 +0200

    Add the entire project

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+// async errors
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+const productsRouter = require('./routes/products');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port: ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
